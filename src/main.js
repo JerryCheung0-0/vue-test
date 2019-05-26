98dd81229a877f590a5ca5537a24d6692650ab83
Jerry Chueng <jerryzshiyan@163.com>
2019-05-26 23:47:56 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastClick from 'fastclick'
+import '@css/reset.css'
+import '@css/border.css'
+import '@css/iconfont.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const port = 8888
+const name = 'vue test' // page title
+
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
+module.exports = {
+    lintOnSave: true,
+    devServer: {
+        port: port,
+        open: true
+    },
+    configureWebpack: {
+        // provide the app's title in webpack's name field, so that
+        // it can be accessed in index.html to inject the correct title.
+        name: name,
+        resolve: {
+            alias: {
+                '@': resolve('src'),
+                '@css': resolve('src/assets/css')
+            }
+        }
+    }
+}
